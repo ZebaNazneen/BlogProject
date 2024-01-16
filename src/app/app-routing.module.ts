6dbf6991f1f321bc963fa9ba549facbd8b36ff73
Zeba Nazneen <zeba1701@gmail.com>
2024-01-16 15:34:25 -0500

    First Commit Showing Example of ROuting

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FirstComponent } from './first/first.component';
+import { SecondComponent } from './second/second.component';
+import { ThirdComponent } from './third/third.component';
 
-const routes: Routes = [];
+const routes: Routes = [ 
+  { path: 'first-component', component: FirstComponent },
+  { path: 'second-component', component: SecondComponent },
+  { path: 'third-component', component: ThirdComponent }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
